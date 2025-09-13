77457de09215a1eccf937db65db81b6626245779
M Deepan Kumar <sivadeepan06@gmail.com>
2025-09-13 13:10:34 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "tailwindcss";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
